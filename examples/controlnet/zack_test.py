f97306f7e8de04768b95316398ad61ffdecdce70
zack zack <2368897913@qq.com>
2024-05-08 14:30:24 +0800

    new

--- examples/controlnet/zack_test.py
+++
@@ -1,9 +0,0 @@
-from datasets import load_dataset
-# dataset = load_dataset(
-#             '/gpfsdata/home/zhangchenkai/download/ADEcolor'
-# )
-dataset = load_dataset(
-            '/gpfsdata/home/zhangchenkai/download/ADEgray'
-)
-column_names = dataset["train"].column_names
-print("column_names",column_names)
--- scripts/zack_1.sh
+++
@@ -1,4 +0,0 @@
-python convert_original_controlnet_to_diffusers.py --checkpoint_path="/gpfsdata/home/zhangchenkai/zack_0_text2mask/ControlNet-main/lightning_logs/version_9616/checkpoints/epoch=16-step=85900.ckpt" \
-    --original_config_file="/gpfsdata/home/zhangchenkai/zack_0_text2mask/ControlNet-main/models/cldm_v15.yaml" \
-    --dump_path="/gpfsdata/home/zhangchenkai/download/ADE_mask2image_control" \
-    --device="cuda:9"
